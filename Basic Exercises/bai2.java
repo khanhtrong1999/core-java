caba6136fd91b6896477daadff2b8b16e189225a
khanhtrong1999 <37130643+khanhtrong1999@users.noreply.github.com>
2018-03-19 16:52:49 +0700

    Add files via upload

---
+++ Basic Exercises/bai2.java
@@ -0,0 +1,8 @@
+
+public class bai2 {
+	public static void main(String[] args) {
+		  System.out.println(74+36);
+		 }
+		 
+
+}
---
+++ Basic Exercises/bai46.java
@@ -0,0 +1,6 @@
+public class bai46 {
+  
+  public static void main(String[] args){
+         System.out.format("\nCurrent Date time: %tc%n\n", System.currentTimeMillis());
+    }
+}
